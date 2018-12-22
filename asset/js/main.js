c4cdd3b730149aa4a9667eab08d68ecb54636894
Felip Barbosa <www@felip.com.br>
2018-12-21 23:56:56 -0200

    layout mobile fix menu

--- asset/js/main.js
+++ asset/js/main.js
@@ -27,12 +27,12 @@ $(document).ready(function () {
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    $('.btn-mobile-menu__icon').toggleClass('fa-bars fa-times animated fadeIn')
   })
 
   $('.navigation-wrapper .blog-button').click(function () {
-    $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
+    $('.btn-mobile-menu__icon').toggleClass('fa-bars fa-times animated fadeIn')
   })
 
 })
